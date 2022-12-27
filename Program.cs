ea7dac71e0feb645330bc7b313a2caacbd8eacd5
Oleg Iraklionov <irakliov@mail.ru>
2022-12-27 23:33:03 +0300

    Case 23 is ok

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и 
+//выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+Console.Write("Введите число N");
+int N = int.Parse(Console.ReadLine()!);
+string VyvodCubov(int N)
+{
+    String res = "";
+    int i =1;
+    while (i<=N)
+    {
+        String res1 = ((Math.Pow(i,3))+",");
+        res = res + res1;
+        i=i+1;
+    }
+return res=res[..^1];
+}
+Console.WriteLine(VyvodCubov(N));
